129e12169b65f736a651b4a4ac0c8db6b0e5e491
anna-moore <ammoore434@gmail.com>
2021-07-12 20:07:02 -0500

    fizzbuzz exericse complete

---
+++ fizzbuzz/csharp/Program.cs
@@ -0,0 +1,34 @@
+//Fizz buzz is a group word game for children to teach them about division.
+// Players take turns to count incrementally, replacing any number divisible by three with the word "fizz",
+// and any number divisible by five with the word "buzz".
+
+using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
